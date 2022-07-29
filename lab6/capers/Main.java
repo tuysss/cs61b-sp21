8113e53304233938cc7ebce00098007ed557d98b
tuysss <2862670249@qq.com>
2022-07-29 16:48:49 +0800

    update lab6

--- lab6/capers/Main.java
+++ lab6/capers/Main.java
@@ -41,7 +41,7 @@ public class Main {
         if (args.length == 0) {
             Utils.exitWithError("Must have at least one argument");
         }
-        System.out.println("args: " + Arrays.toString(args));
+        //System.out.println("args: " + Arrays.toString(args));
         CapersRepository.setupPersistence();
         String text;
         switch (args[0]) {
@@ -53,12 +53,10 @@ public class Main {
             break;
         case "dog":
             validateNumArgs("dog", args, 4);
-            // TODO: make a dog
             CapersRepository.makeDog(args[1],args[2],Integer.parseInt(args[3]));
             break;
         case "birthday":
             validateNumArgs("birthday", args, 2);
-            // TODO: celebrate this dog's birthday
             CapersRepository.celebrateBirthday(args[1]);
             break;
         default:
