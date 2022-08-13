b8141e5521636fa6fecbb774cb42c862d4e01cfb
tuysss <2862670249@qq.com>
2022-08-13 18:27:49 +0800

    update Blob

--- proj2/gitlet/Blob.java
+++ proj2/gitlet/Blob.java
@@ -2,6 +2,8 @@ package gitlet;
 
 import java.io.File;
 import java.io.Serializable;
+import static gitlet.MyUtils.*;
+import static gitlet.Utils.*;
 
 /**
  *  represent a gitlet Blob object.
@@ -12,40 +14,64 @@ public class Blob implements Serializable {
     /** the global unique SHA-1 id of blob */
     private final String hashID;
 
-    /** the name of the modified file */
-    private final String name;
+    /**
+     * The source file from constructor.
+     * source points to file in CWD.
+     */
+    private final File source;
 
     /**  store the content which read from file as byte[] */
     private final byte[] content;
 
-    /**  store the content which read from file as String */
-    private final String contentAsString;
+    /**
+     * The file of this instance with the path generated from SHA1 id.
+     * File points to file in .gitlet/objects
+     */
+    private final File file;
 
     /**
-     * Constructor of Blob
-     * @param name name of the modified file
+     * Constructor of Blob.
+     * @param sourceFile
      */
-    public Blob(String name){
-        File file=new File(name);
-        this.name=name;
-        this.content=Utils.readContents(file);
-        this.contentAsString=Utils.readContentsAsString(file);
-        hashID=Utils.sha1(name,contentAsString);
+    public Blob(File sourceFile){
+        this.source=sourceFile;
+        this.content=readContents(source);
+        String filePath=sourceFile.getPath();
+        this.hashID=sha1(filePath,content);
+        //尚未持久化，只是指向file对象
+        this.file=getObjectFromFile(hashID);
+    }
+
+    /**
+     * Save blob object into ./gitlet/objects
+     */
+    public void save(){
+        saveObjectToFile(file,this);
+
+    }
+
+    /**
+     * Get a blob obj from file with uid.
+     * @param hashID
+     * @return
+     */
+    public Blob fromFile(String hashID){
+        return readObject(getObjectFromFile(hashID),Blob.class);
     }
 
     public String getHashID() {
         return hashID;
     }
 
-    public String getName() {
-        return name;
+    public File getSource() {
+        return source;
     }
 
     public byte[] getContent() {
         return content;
     }
 
-    public String getContentAsString() {
-        return contentAsString;
+    public File getFile() {
+        return file;
     }
 }
