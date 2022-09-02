0f4eebafa7c61234f3c57eb1ed5de6e4fdb5acc1
tuysss <2862670249@qq.com>
2022-09-02 17:11:24 +0800

    finish checkout

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -48,7 +48,36 @@ public class Main {
                 repo.checkIfInitDirectoryExists();
                 repo.global_log();
                 break;
-
+            case "find":
+                repo.checkCommandLength(args.length, 2);
+                repo.checkIfInitDirectoryExists();
+                repo.find(args[1]);
+                break;
+            case "status":
+                repo.checkCommandLength(args.length, 1);
+                repo.checkIfInitDirectoryExists();
+                repo.status();
+                break;
+            case "checkout":
+                int len = args.length;
+                if (len < 2 || len > 4) {
+                    System.out.println("Incorrect operands.");
+                    System.exit(0);
+                }
+                repo.checkIfInitDirectoryExists();
+                if (len == 2) {
+                    // java gitlet.Main checkout [branch name]
+                    repo.checkoutBranch(args[1]);
+                } else if (len == 3) {
+                    // java gitlet.Main checkout -- [file name]
+                    repo.checkEqual(args[1], "--");
+                    repo.checkoutFile(args[2]);
+                } else if (len == 4) {
+                    // java gitlet.Main checkout [commit id] -- [file name]
+                    repo.checkEqual(args[2], "--");
+                    repo.checkoutFileWithCommitId(args[1], args[3]);
+                }
+                break;
             default:
                 System.out.println("No command with that name exists.");
                 System.exit(0);
